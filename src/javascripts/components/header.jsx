57d27973018dc4a5e17de7c73756382da2cc8a47
Gerson Lima <glima@futurebrand.com>
2019-07-29 11:15:37 -0300

    header link example

--- src/javascripts/components/header.jsx
+++ src/javascripts/components/header.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   render() {
     return (
       <header className="header">
         <p>Header here</p>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+            <Link to="/contato">Contato</Link>
+          </li>
+        </ul>
       </header>
     );
   }
